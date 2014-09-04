772e343bb720609af06eb8a4627cff96a56fac70
sh0hei <vividbit@gmail.com>
2014-09-05 00:14:44 +0900

    Add sample.json

--- src/main/scala/com/github/sh0hei/mascalade/coderwall/CoderwallClient.scala
+++ src/main/scala/com/github/sh0hei/mascalade/coderwall/CoderwallClient.scala
@@ -1,4 +1,10 @@
 package com.github.sh0hei.mascalade.coderwall
 
-class CoderwallClient() extends CoderWallClient() {
+import scala.io._
+import org.json4s._
+
+object CoderwallClient() {
+  val API_BASE_URL = "https://www.coderwall.com"
+  val FORMAT = ".json"
+  val ENV_API_KEY = "API_KEY"
 }
--- src/main/scala/com/github/sh0hei/mascalade/coderwall/CoderwallClientBase.scala
+++
@@ -1,6 +0,0 @@
-package com.github.sh0hei.mascalade.coderwall
-
-abstract class CoderwallClientBase() {
-	val api_url = "https://www.coderwall.com"
-	val format = ".json"
-}
